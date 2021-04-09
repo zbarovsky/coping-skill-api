cdfa223943a545c4b811e5f269ee0350f73aa03b
zbarovsky <zachary.barovsky@gmail.com>
2021-04-09 10:35:36 -0700

    created landing page. tested, flask app runs correctly

--- api.py
+++ api.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, url_for, request, redirect
+from skills import coping_skills
 
 app = Flask(__name__)
 
+# landing page for API
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+# show all coping skills
+@app.route('/api/get_all', methods=['GET'])
+def get_all():
+    pass
+
+# get coping skill by id passed from frontend
 @app.route('/api/<int:id>', methods=['GET'])
 def get_skill(id):
     pass
