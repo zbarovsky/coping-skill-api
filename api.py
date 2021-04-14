f12adeb0078ff22a7acb430960ba8ee0c7679a1d
zbarovsky <zachary.barovsky@gmail.com>
2021-04-14 13:24:59 -0700

    updated routing so that query params worked properly. Also set up catch to make sure input was a valid number

--- api.py
+++ api.py
@@ -9,14 +9,22 @@ def index():
     return render_template('index.html')
 
 # show all coping skills
-@app.route('/api/get_all', methods=['GET'])
+@app.route('/api/copingskill/get_all', methods=['GET'])
 def get_all():
     return jsonify(coping_skills)
 
 # get coping skill by id passed from frontend
-@app.route('/api/<int:id>', methods=['GET'])
-def get_skill(id):
-    return jsonify(coping_skills[id])
+@app.route('/api/copingskill/', methods=['GET'])
+def get_skill():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided."
+
+    if id not in range(len(coping_skills)):
+        return "Error, invalid id"
+    else: 
+        return jsonify(coping_skills[id])
 
 if __name__ == '__main__':
     app.run(debug=True)
